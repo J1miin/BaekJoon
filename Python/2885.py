45bf23add8332cddfc7c092e303ad17342a1ef97
Jimin <gift040404@naver.com>
2024-08-06 15:51:42 +0900

    add comment

--- Python/2885.py
+++ Python/2885.py
@@ -33,10 +33,10 @@ else :
         if iK >= iKmok :
             iK -= iKmok
             iCutCnt +=1
-            #print(iK, iKmok, iCutCnt)
+            print(iK, iKmok, iCutCnt)
         else : 
             iCutCnt +=1
-            #print("else ans")
+            #print("else ans") 5의 경우 2개짜리 조각은 필요없고 (4+1) 로 1개짜리 조각이 필요해서 그런 경우 자르기만 하니까 이 else문이 필요
         
-
+#K를 K보다 작고, 사이즈보다 작은 수(가장 작은 조합으로)의 합으로 구성하는 문제이다. 그래서 K가 0이될때까지 찾아주면 되는것!
 print(iSize, iCutCnt)
