bb828e2a165583b64c097a3b1df26a9772498652
Jimin <gift040404@naver.com>
2024-08-13 12:36:52 +0900

    string) 27497 fixed

--- Python/27497.py
+++ Python/27497.py
@@ -1,42 +1,44 @@
 #알파벳 블록
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
 iPlayNum = int(input())
-sResultArr = []
+sResultArr = deque()  # 덱으로 변경해야 하는 것 같다...ㅠ
 charSequenceArr = []
 
-def blockGame(sInputList, seq):
-    if sInputList[0] == '1' :
-        sResultArr.append([sInputList[1],seq])
-        charSequenceArr.append(sInputList[1])
-    elif sInputList[0] == '2' : #맨앞에 추가
-        sResultArr.insert(0,[sInputList[1],seq])
-        charSequenceArr.append(sInputList[1])
-    elif sInputList[0] == '3' :
-        tempArr=[]
-        if sResultArr == False :
-            pass
-        for j in range(len(sResultArr)):
-            for k in range(len(sResultArr)):
-                if charSequenceArr[len(charSequenceArr)-j-1] == sResultArr[k][0] :
-                    #같은게 있을 수 있으니까
-                    tempArr.append(sResultArr[k])
-        if tempArr :
-            tempArr.sort(key=lambda x : int(x[1]) ,reverse=True)
-            sResultArr.remove(tempArr[0])
-            
-def printResult(arr):
-    result=[]
-    if sResultArr :
-        for i in range(len(arr)):
-            result.append(arr[i][0])
-        print(''.join(result))
-    else :
+#덱이 아니라 리스트를 쓰면 insert하고 pop하는데 O(n)의 시간이 걸린다.
+#But 덱을 쓰면 O(1)의 시간 복잡도로 동일 작업을 할 수 있게 된다.
+
+def blockGame(sInputList):
+    #charSequenceArr에 들어온 순서가 아니라 메뉴 번호를 넣어서 앞에서 지울지 뒤에서 지울지를 정하면 된다!
+    if sInputList[0] == '1':
+        sResultArr.append(sInputList[1])  
+        charSequenceArr.append(sInputList[0])
+        
+    elif sInputList[0] == '2':  # 맨앞에 추가
+        sResultArr.appendleft(sInputList[1])  
+        charSequenceArr.append(sInputList[0])
+
+    elif sInputList[0] == '3':
+        if charSequenceArr:
+            sMenu = charSequenceArr.pop()
+            if sMenu == '1':
+                sResultArr.pop()  
+            elif sMenu == '2':
+                sResultArr.popleft()  
+  
+##########
+def printResult():
+    if sResultArr:
+        print(''.join(sResultArr))  # 덱의 요소를 모두 출력
+    else:
         print(0)
 ##################
-for i in range(iPlayNum):
-    sInputList = input().split()
-    blockGame(sInputList,i)
 
-printResult(sResultArr)
+for _ in range(iPlayNum):
+    sInputList = input().split()
+    blockGame(sInputList)
+
+printResult()
