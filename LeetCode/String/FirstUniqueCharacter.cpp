94f1e0858ea82d71a453e6e521f93fb92b4613c7
Jimin <gift040404@naver.com>
2025-05-03 22:39:33 +0900

    Update FirstUniqueCharacter.cpp

--- LeetCode/String/FirstUniqueCharacter.cpp
+++ LeetCode/String/FirstUniqueCharacter.cpp
@@ -3,8 +3,22 @@
 #include <unordered_map>
 #include <string>
 using namespace std;
-
 class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> v(27,0);
+        for (int i=0;i<s.size();++i){
+            v[s[i]-'a']++;
+        }
+        for (int i=0;i<s.size();++i){
+            if (v[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+class Solution2 {
 public:
     int firstUniqChar(string s) {
         unordered_map<int,int> hMap; 
