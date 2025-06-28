b33cebeb28b7db9d3871289ac63d702ac2f9c6e1
Jimin <gift040404@naver.com>
2025-06-29 00:14:08 +0900

    [TCD] si1_v2
    if 조건문을 min을 이용해서 간단하게 만들기

--- C++/TCD/si1.cpp
+++ C++/TCD/si1.cpp
@@ -12,30 +12,22 @@ void isAvailiable(){
     int available = 0;
     for(int i=0;i<fromId.size();++i){
         available = capacities[toId[i]] - bottles[toId[i]];    
-        //cout<<"i "<<i <<": availalbe "<<available<<endl;
-        if (available>=bottles[fromId[i]]) {
-            bottles[toId[i]]+= bottles[fromId[i]];
-            bottles[fromId[i]]=0;
-            //cout<<"if "<<i <<": bottles[fromId[i]] "<<bottles[fromId[i]]<<endl;
-        }else{
-            bottles[toId[i]]=capacities[toId[i]];
-            bottles[fromId[i]] = bottles[fromId[i]]-available;
-            //cout<<"else "<<i <<": bottles[fromId[i]] "<<bottles[fromId[i]]<<endl;
-        }
-
-        // for(auto a:bottles){
-        //     cout<<a<<" ";
+        int gap = min(bottles[fromId[i]],available);
+        bottles[fromId[i]]-=gap;
+        bottles[toId[i]]+=gap;
+        
+        // if (available>=bottles[fromId[i]]) {
+        //     bottles[toId[i]]+= bottles[fromId[i]];
+        //     bottles[fromId[i]]=0;
+        // }else{
+        //     bottles[toId[i]]=capacities[toId[i]];
+        //     bottles[fromId[i]] = bottles[fromId[i]]-available;
         // }
-        // cout<<endl;
+        
     }
 
 }
 int main(){
-    // capacities = {14,35,86,58,25,62};
-    // bottles = {6,34,27,38,9,60};
-    // fromId={1,2,4,5,3,3,1,0};
-    // toId={0,1,2,4,2,5,3,1};
-
     capacities = {700000,800000,900000,1000000};
     bottles = {478478, 478478, 478478, 478478};
     fromId={2,3,2,0,1};
