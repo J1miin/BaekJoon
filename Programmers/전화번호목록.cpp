2c369c7f834abb1ad7f04495a2ef8e177320914e
Jimin <gift040404@naver.com>
2025-07-03 00:41:39 +0900

    [프로그래머스] 전화번호목록 수정
    시간초과 수정 >> 해시맵 & substr 이용

--- Programmers/전화번호목록.cpp
+++ Programmers/전화번호목록.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 using namespace std;
 
+//시간초과
 bool solution(vector<string> phone_book) {
     bool answer = true;
     for (int i=0;i<phone_book.size();++i){
@@ -11,6 +12,33 @@ bool solution(vector<string> phone_book) {
             if ( phone_book[i].find(phone_book[j])==0){
                 answer = false; 
                 break;
+            }
+        }
+    }
+    return answer;
+}
+
+//해시와 substr이용하기
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+bool solution2(vector<string> phone_book) {
+    bool answer = true;
+    unordered_map<string,int> hMap;
+    for(auto a: phone_book){
+        hMap[a]++;
+    }
+
+    for (int i=0;i<phone_book.size();++i){
+        for(int j=0;j<phone_book[i].size()-1;++j){
+            //자기 자신 제외하기 위해서 size()-1
+            string checkString = phone_book[i].substr(0,j+1);
+            if (hMap[checkString]) {
+                answer = false;
+                return answer;
             }
         }
     }
